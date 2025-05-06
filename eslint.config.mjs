7704f3ad02a2c7d45eaab1aca00c41b7352b2c71
Fauzer Junior <fauzerjunnior@hotmail.com>
2025-05-06 15:41:47 -0300

    feat: init commit with CRUD using database

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'class-methods-use-this': 'off',
+    },
+  },
+]);
---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+});
+
+client.connect();
+
+exports.query = async (param, values) => {
+  const { rows } = await client.query(param, values);
+
+  return rows;
+};
