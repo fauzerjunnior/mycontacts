22f8458887183113fbb580ff56922f8298c7d302
Fauzer Junior <fauzerjunnior@hotmail.com>
2025-05-09 13:45:48 -0300

    chore: add web application

---
+++ web/eslint.config.mjs
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'import/no-anonymous-default-export': 'off',
+      'jsx-a11y/alt-text': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  prettierConfig,
+];
